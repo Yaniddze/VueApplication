1d79aeb20992bfda53186039dbd97e535b0073d5
Yaniddze <FaceCrack1337@gmail.com>
2020-09-03 17:01:03 +0500

    Refactor components

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,10 @@
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 import Counter from './components/Counter.vue';
 
-export default ({
+export default defineComponent({
   name: 'App',
   components: {
     Counter,
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -13,7 +13,9 @@
 </template>
 
 <script lang="ts">
-export default ({
+import { defineComponent } from 'vue';
+
+export default defineComponent({
   name: 'Counter',
   data() {
     return {
